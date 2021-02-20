accffd7d20667a05e7b9432277a0f073bc263b2f
Orion Belete <78234783+Obelete@users.noreply.github.com>
2021-02-20 10:52:43 -0500

    CMSC203_Lab3
    introduced to multiple classes

---
+++ MovieDriver.java
@@ -0,0 +1,29 @@
+package MovieDriverPackage;
+import java.util.Scanner;
+public class MovieDriver 
+{
+
+	public static void main(String[] args) 
+	{
+		String answer;
+		do
+		{
+			Scanner kb = new Scanner(System.in);
+			Movie movie = new Movie();
+			System.out.println("Enter the title of a movie: ");
+			String movieTitle = kb.nextLine();
+			movie.setTitle(movieTitle);
+			System.out.println("Enter the movie's rating: ");
+			String movieRating = kb.nextLine();
+			movie.setRating(movieRating);
+			System.out.println("Enter the number of tickes sold at the theater: ");
+			int movieTicketSold = kb.nextInt();
+			movie.setSoldTickets(movieTicketSold);
+			System.out.println(movie.toString());
+			kb.nextLine();
+			System.out.println("DO you want to enter another(y or no)");
+			answer = kb.nextLine();
+		}while (answer.equalsIgnoreCase("y"));
+	}
+
+}
